defc931600cbe0cb0162ece9731b8ceb01d8b396
rnpacifique <rpacifique12@gmail.com>
2023-11-04 02:27:31 -0700

    a program that prints the ASCII alphabet, in lowercase,

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for i in range(97,123):
-    if (i != 113) and (i != 101)
-      print("{:c}".format(i), end="")
+for letter in range(97,123):
+    if (letter != 113) and (letter != 101)
+      print("{:c}".format(letter), end="")
