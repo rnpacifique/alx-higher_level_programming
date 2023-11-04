3041b955463fcf6e0a5949dd5e9ffb19231bec5e
rnpacifique <rpacifique12@gmail.com>
2023-11-04 03:00:45 -0700

    a program that prints all numbers from 0 to 98 in decimal and in hexadecimal

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
-for letter in range(97,223):
-    if chr(letter) != 113 and letter != 101:
+for letter in range(ord('a'), ord('z') + 1):
+    if letter != ord('q') and letter != ord('e'):
         print("{:c}".format(letter), end="")
