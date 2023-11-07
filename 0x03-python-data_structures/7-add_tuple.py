263917129ce3ea2ff714472b170b2cb3027905a7
rnpacifique <rpacifique12@gmail.com>
2023-11-07 13:50:54 -0800

    a function that adds 2 tuples.

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,9 +1,4 @@
-#!/usr/bin/python3
-
-def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        for idx, num in enumerate(row):
-            print("{:d}".format(num), end="")
-            if idx < len(row) - 1:
-                print(" ", end="")
-        print()
+def add_tuple(tuple_a=(), tuple_b=()):
+    a = tuple_a + (0, 0)
+    b = tuple_b + (0, 0)
+    return (a[0] + b[0], a[1] + b[1])
