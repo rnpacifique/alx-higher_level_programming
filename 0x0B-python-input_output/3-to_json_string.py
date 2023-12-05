ad6ee60b77cc504566af8c9c7c2122f49df93c41
rnpacifique <rpacifique12@gmail.com>
2023-12-05 11:46:48 -0800

    a function that returns an object (Python data structure) represented by a JSON string:

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""this module returns json representation"""
+import json
+
+
+def to_json_string(my_obj):
+    """this function returns the JSON"""
+    return json.dumps(my_obj)
---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""this module loads json"""
+import json
+
+
+def from_json_string(my_str):
+    """thsi function returns a data structure"""
+    return json.loads(my_str)
