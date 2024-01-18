c31d3657d736fb49e3029528b1a4293eba5f075a
rnpacifique <rpacifique12@gmail.com>
2024-01-18 11:48:02 -0800

    a script that lists all records of the table second_table

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,3 +1,5 @@
--- this select rows with names
-SELECT score, name FROM second_table
-WHERE name IS NOT NULL;
+-- A  script that lists all records of the table second_table
+SELECT score, name
+FROM second_table
+WHERE name IS NOT NULL
+ORDER BY score DESC;
