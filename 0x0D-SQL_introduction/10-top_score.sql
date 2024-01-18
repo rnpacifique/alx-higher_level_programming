a70153bb90b539bd3c374df3e996f31cd4bd1b34
rnpacifique <rpacifique12@gmail.com>
2024-01-18 11:10:37 -0800

    a script that lists all records of the table second_table

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,2 +1,4 @@
--- select only name and score
-SELECT (name, score) FROM second_table ORDER BY score DESC;
+-- A script that lists all records of the table second_table
+SELECT score, name
+FROM second_table
+ORDER BY score DESC;
