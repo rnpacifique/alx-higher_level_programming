09591665bcb197633fdd926247da9935880498cd
rnpacifique <rpacfique12@gmail.com>
2024-01-21 17:24:18 +0200

    create database

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,4 +1,7 @@
  -- ascript that create database 
- CREATE DATABASE IF NOT EXITS btn_0d_usa ( 
-    CREATE TABLE states (PRIMARY KEY id INT INIQUE, name VARCHAR(256) )
- )
+ CREATE DATABASE IF NOT EXITS btn_0d_usa;
+ USE btn_0d_u
+ CREATE TABLE IF NOT EXISTS states (
+    id INT NOT NULL INIQUE PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(256) NOT NULL
+ );
