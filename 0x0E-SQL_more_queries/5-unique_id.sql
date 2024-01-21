ffaf9399ebf3cb8eb6d430e0c88996af1e395ca1
rnpacifique <rpacfique12@gmail.com>
2024-01-21 17:34:03 +0200

    creste database

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,2 +1,2 @@
--- script that create table 
-CREATE TABLE IF DOES NOT EXITS unique_id (id INT DEFAULT 1 UNIQUE, name = VARCHAR(256))
+-- A script that creates the table unique_id on your MySQL server
+CREATE TABLE IF NOT EXISTS unique_id (id INT DEFAULT 1 UNIQUE, name VARCHAR(256));
--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,6 +1,6 @@
  -- ascript that create database 
- CREATE DATABASE IF NOT EXITS btn_0d_usa;
- USE btn_0d_u
+ CREATE DATABASE IF NOT EXITS hbtn_0d_usa;
+ USE hbtn_0d_u
  CREATE TABLE IF NOT EXISTS states (
     id INT NOT NULL INIQUE PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(256) NOT NULL
