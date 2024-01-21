41c02def3ce44462828845ec3f1f5a062aabe81f
rnpacifique <rpacfique12@gmail.com>
2024-01-21 16:04:22 +0200

    create database

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,2 +1,2 @@
 -- script that create table 
-CREATE TABLE IF DOES NOT EXITS unique_id (id INT = 1 UNIQUE, name = VARCHAR(256))
+CREATE TABLE IF DOES NOT EXITS unique_id (id INT DEFAULT 1 UNIQUE, name = VARCHAR(256))
---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,4 @@
+ -- ascript that create database 
+ CREATE DATABASE IF NOT EXITS btn_0d_usa ( 
+    CREATE TABLE states (PRIMARY KEY id INT INIQUE, name VARCHAR(256) )
+ )
