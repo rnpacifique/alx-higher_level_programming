68f416810ec34a08a764429d3770bd9f69d04835
rnpacifique <rpacfique12@gmail.com>
2024-02-22 20:01:06 +0200

    as cript that list all states in data base

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,28 +1,13 @@
 #!/usr/bin/python3
-"""this connect to adata base and aexecute sql queries"""
-import MySQLdb
+# Lists all states from the database hbtn_0e_0_usa.
+# Usage: ./0-select_states.py <mysql username> \
+#                             <mysql password> \
+#                             <database name>
 import sys
+import MySQLdb
 
 if __name__ == "__main__":
-    try:
-        db = MySQLdb.connect(
-            host="localhost",
-            user=sys.argv[1],
-            passwd=sys.argv[2],
-            db=sys.argv[3],
-            port=3306)
-        cur = db.cursor()
-        cur.execute("SELECT * FROM states ORDER BY states.id")
-        rows = cur.fetchall()
-        for row in rows:
-            print(row)
-    except MySQLdb.Error as e:
-        try:
-            print("MySQL error [%d]: [%s]" % (e.args[0], e.args[1]))
-        except IndexError:
-            print("Mysql error: [%s]" % str(e))
-    finally:
-        if "cur" in locals() and cur is not None:
-            cur.close()
-        if "db" in locals() and db is not None:
-            db.close()
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM `states`")
+    [print(state) for state in c.fetchall()]
