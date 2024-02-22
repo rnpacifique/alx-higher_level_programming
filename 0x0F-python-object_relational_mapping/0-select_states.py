f7dc25d393b90d5b32da2e9fdc22950bcb531ea7
rnpacifique <rpacfique12@gmail.com>
2024-02-22 21:02:59 +0200

    task

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,13 +1,40 @@
 #!/usr/bin/python3
-# Lists all states from the database hbtn_0e_0_usa.
-# Usage: ./0-select_states.py <mysql username> \
-#                             <mysql password> \
-#                             <database name>
+"""
+This is the script to execute task 0.
+"""
 import sys
 import MySQLdb
 
+
+def main(username, password, database):
+    """
+    Function to select all states from database hbtn_0e_0_usa.
+    """
+
+    db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=username,
+            passwd=password,
+            db=database)
+
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY id")
+
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    db.close()
+
+
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c = db.cursor()
-    c.execute("SELECT * FROM `states`")
-    [print(state) for state in c.fetchall()]
+    if len(sys.argv) != 4:
+        print("Usage: ./0-select_states.py <username> <password> <database>")
+        sys.exit(1)
+
+    username, password, database = sys.argv[1:]
+
+    main(username, password, database)
