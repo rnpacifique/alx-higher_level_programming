76ab36496cfb6a2eac8e74331263752db5a4aac7
rnpacifique <rpacfique12@gmail.com>
2024-02-22 21:16:12 +0200

    task

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,17 +1,43 @@
 #!/usr/bin/python3
-# Displays all values in the states table of the database hbtn_0e_0_usa
-# whose name matches that supplied as argument.
-# Usage: ./2-my_filter_states.py <mysql username> \
-#                                <mysql password> \
-#                                <database name> \
-#                                <state name searched>
+"""
+Script to list all  states with name that matching name given.
+"""
 import sys
 import MySQLdb
 
+
+def main(username, password, database, sname):
+    """
+    Function to select all states from database hbtn_0e_0_usa
+    with name equal to name given as an argument.
+    """
+
+    db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=username,
+            passwd=password,
+            db=database)
+
+    cursor = db.cursor()
+    cursor.execute(
+            "SELECT * FROM states WHERE name = '{}' \
+                    ORDER BY id ASC".format(sname))
+
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    db.close()
+
+
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c = db.cursor()
-    c.execute("SELECT * \
-                 FROM `states` \
-                WHERE BINARY `name` = '{}'".format(sys.argv[4]))
-    [print(state) for state in c.fetchall()]
+    if len(sys.argv) != 5:
+        print("Usage: ./2-my_filter_states.py <username> \
+                <password> <database> <sname>")
+        sys.exit(1)
+
+    username, password, database, sname = sys.argv[1:]
+    main(username, password, database, sname)
