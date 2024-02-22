cc6556b7ada5123ba37c2a28fce09c389992bba6
rnpacifique <rpacfique12@gmail.com>
2024-02-22 21:12:25 +0200

    task

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,13 +1,41 @@
 #!/usr/bin/python3
-# Lists all states with a name starting with N from the database hbtn_0e_0_usa.
-# Usage: ./1-filter_states.py <mysql username> \
-#                             <mysql password> \
-#                             <database name>
+"""
+Script to select all states from the database.
+"""
+
 import sys
 import MySQLdb
 
+
+def main(username, password, database):
+    """
+    Function to select all states from database hbtn_0e_0_usa
+    where name starts with N.
+    """
+
+    db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=username,
+            passwd=password,
+            db=database)
+
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' \
+            ORDER BY id ASC")
+
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    db.close()
+
+
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c = db.cursor()
-    c.execute("SELECT * FROM `states` ORDER BY `id`")
-    [print(state) for state in c.fetchall() if state[1][0] == "N"]
+    if len(sys.argv) != 4:
+        print("Argments must be 3")
+        sys.exit(1)
+
+    username, password, database = sys.argv[1:]
+    main(username, password, database)
