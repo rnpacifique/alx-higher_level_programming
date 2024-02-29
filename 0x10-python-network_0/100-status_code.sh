d8efb7e297883a12d671e2fe7facb4a2951d81e5
rnpacifique <rpacifique12@gmail.com>
2024-02-29 14:44:45 -0800

    task

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# A Bash script that sends a request to a URL passed as an argument
-(echo -n $(curl -s -o /dev/null -w "%{http_code}" "$1"))
+# Sends a GET and also display the response status code.
+curl -s -o /dev/null -w "%{http_code}" "$1"
