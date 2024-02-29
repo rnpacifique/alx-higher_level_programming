f4f18f162d6ec2e4e928195c3cefa0b7bdde6452
rnpacifique <rpacifique12@gmail.com>
2024-02-29 14:25:20 -0800

    task

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# displays the body on as 200
-curl -sL "$1"
+# A Bash script that takes in a URL, sends a request to that URL
+curl -s "$1" -w "%{size_download}\n" -o /dev/null
---
+++ 0x10-python-network_0/1-body.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Use curl to send a GET request to the provided URL and display the body for a 200 status code
+curl -sL "$1"
